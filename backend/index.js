eab57673da7fe506f6b8f7223b80f908e6ff9322
mtk2024 <drmichael1519@gmail.com>
2024-09-23 20:41:05 -0400

    Added loading previous data from other room users on refresh or joinRoom

--- backend/index.js
+++ backend/index.js
@@ -11,30 +11,51 @@ const io = socketIo(server, {
   }
 });
 
+const roomContent = {};  // Store the content of each room
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Handle creating a room
   socket.on('createRoom', (roomId) => {
     console.log(`Creating room: ${roomId}`);
-    socket.join(roomId);  // The user is placed in their own room
-    socket.emit('roomCreated', roomId);  // Notify the user that the room has been created
+    socket.join(roomId);  // User joins their own room
+    socket.emit('roomCreated', roomId);  // Notify the user that the room is created
+
+    // If room has existing content, send it to the user who created the room
+    if (roomContent[roomId]) {
+      socket.emit('codeUpdate', roomContent[roomId]);  // Send the existing content to the creator
+    } else {
+      roomContent[roomId] = '';  // Initialize the room content if it doesn't exist
+    }
   });
 
-  // When a user joins an existing room
+  // Handle joining an existing room
   socket.on('joinRoom', (roomId) => {
     console.log(`User ${socket.id} joining room: ${roomId}`);
-    socket.join(roomId);  // Another user joins the room
+    socket.join(roomId);  // Join the room
+
+    // Notify the room that a new user has joined
     io.to(roomId).emit('userJoined', `${socket.id} has joined the room`);
+
+    // Send the current content of the room to the new user
+    if (roomContent[roomId]) {
+      socket.emit('codeUpdate', roomContent[roomId]);  // Send only to the new joiner
+    }
   });
 
+  // Handle code updates
+  socket.on('codeUpdate', (data, roomId) => {
+    // Update the content for this room
+    roomContent[roomId] = data;
+
+    // Broadcast the code update to all other users in the room
+    socket.to(roomId).emit('codeUpdate', data);
+  });
+
+  // Handle user disconnecting
   socket.on('disconnect', () => {
     console.log('User disconnected');
-  });
-
-  // Handle script updates here
-  socket.on('codeUpdate', (data, roomId) => {
-    // Broadcast the code update to all other connected clients
-    socket.to(roomId).emit('codeUpdate', data);
   });
 });
 
